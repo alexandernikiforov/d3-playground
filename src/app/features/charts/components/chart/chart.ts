a4bab3f41bdcc036011e64d6e2d24fa492fe9d22
Alexander Nikiforov <alexander.nikiforov@gmail.com>
2025-09-23 00:10:39 +0200

    Integrate D3.js into the Chart component and update styles
    - Replaced placeholder HTML in `chart.html` with an SVG element for rendering D3 visualizations.
    - Implemented a basic D3 chart in `chart.ts` with sample data.
    - Added `@types/d3` as a development dependency.
    - Adjusted `.editorconfig` and project code style settings for consistency.

--- src/app/features/charts/components/chart/chart.ts
+++ src/app/features/charts/components/chart/chart.ts
@@ -1,11 +1,37 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, viewChild} from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
-  selector: 'd3p-chart',
-  imports: [],
-  templateUrl: './chart.html',
-  styleUrl: './chart.scss'
+    selector: 'd3p-chart',
+    imports: [],
+    templateUrl: './chart.html',
+    styleUrl: './chart.scss'
 })
-export class Chart {
+export class Chart implements AfterViewInit {
+    chartContainer = viewChild.required<ElementRef<HTMLElement>>('chart');
 
+    ngAfterViewInit(): void {
+        this.createChart();
+    }
+
+    private createChart() {
+        const element = this.chartContainer().nativeElement;
+
+        const data = [
+            {name: "Jim", votes: 12},
+            {name: "Sue", votes: 5},
+            {name: "Bob", votes: 21},
+            {name: "Ann", votes: 17},
+            {name: "Dan", votes: 3}
+        ];
+
+        d3.select(element)
+            .selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("r", 100)
+            .attr("fill", "blue")
+            .attr("transform", "translate(150, 140)")
+    }
 }
